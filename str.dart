0a0a9bd3d6ad6103d513766c690abe81eff72696
Adenike-Wealth <Adesanminikky97@gmail.com>
2023-07-15 15:08:51 +0100

    Day1:15/7/2023

---
+++ str.dart
@@ -0,0 +1,10 @@
+void main(){
+  String name = 'Wealth';
+  String surname = 'Adenike';
+  print('name: $name, surname:$surname');
+  print("Wealth's parent stays in Abuja");
+  print('Wealth\s parent stays in China');
+  print('my clothes cost \$10');
+  print('it is a journey that requires\n'
+  'disciplines');
+}

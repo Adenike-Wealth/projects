32627256af9a7a5161d3a2331c7df2f923a300c0
Adenike-Wealth <Adesanminikky97@gmail.com>
2023-07-15 23:04:21 +0100

    Day1:15/7/2023

--- str.dart
+++ str.dart
@@ -1,10 +1,27 @@
 void main(){
-  String name = 'Wealth';
-  String surname = 'Adenike';
-  print('name: $name, surname:$surname');
+  String name = 'Paul';
+  int age = 35;
+  int price = 400;
+  print("Paul, a 35 years old, paid \$400 to repairs his father's car");
+  
+
+  
+  
   print("Wealth's parent stays in Abuja");
   print('Wealth\s parent stays in China');
   print('my clothes cost \$10');
   print('it is a journey that requires\n'
   'disciplines');
+
+
+  
+
+
+
 }
+
+
+
+
+
+

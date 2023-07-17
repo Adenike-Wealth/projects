fd29997563016811d7b7e45c2c504125f46f7231
Adenike-Wealth <Adesanminikky97@gmail.com>
2023-07-17 12:01:52 +0100

    day2:17/07/2023

--- str.dart
+++ str.dart
@@ -3,15 +3,17 @@ void main(){
   int age = 35;
   int price = 400;
   print("Paul, a 35 years old, paid \$400 to repairs his father's car");
-  
-
-  
-  
   print("Wealth's parent stays in Abuja");
   print('Wealth\s parent stays in China');
   print('my clothes cost \$10');
   print('it is a journey that requires\n'
   'disciplines');
+
+  double tempCityEarly = 18.5;
+  double tempCityLate = 25.7;
+  print('The average temperature for the day is ${(tempCityEarly + tempCityLate)/2}degrees celcius}');
+
+
 
 
   
